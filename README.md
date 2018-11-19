8f3fb3dbd92eaaa581ace04df8d5cbc93e332b10
ryb413516 <1259821073@qq.com>
2018-11-19 17:04:55 +0800

    面向对象优化

--- README.md
+++ README.md
@@ -13,20 +13,23 @@ PC端实现涂抹擦著效果,超过50%的涂抹面积可以查看全部,涂抹�
 
 使用步骤说明:
 1.在HTML中添加指定id的canvas标签。
-例如:<canvas id="cas" width="375" height="667"></canvas>
+例如:
+``` 
+	<canvas id="cas" width="375" height="667"></canvas> ```
 
 2.编写配置文件
-| 属性名 | 取值类型 | 备注 |
-| id | 字符串 | canvas标签的id |
-| coverType | 字符串 | 取值"color"或"image" |
-| color | 字符串 | 十六进制颜色码,或rgba(),如果不指定默认值为 #666 |
-| imgUrl | 字符串 | 前面的覆盖图片 |
-| backImgUrl | 字符串 | canvas背景图片 |
-| width | 字符串 | canvas的宽度，必需和canvas标签中宽度一致 |
-| height | 字符串 | canvas的高度，必需和canvas标签中高度一致 |
-| radius | 字符串 | 涂抹笔的半径 |
-| percent | 数值 | 透明面积占整个画布的百分比，超出此数字显示全部画布 |
-| callback | 函数 | 用户自定义的回调函数名称，必填 |
+|| 属性名 | 取值类型 | 备注 ||
+|| id | 字符串 | canvas标签的id ||
+|| coverType | 字符串 | 取值"color"或"image" ||
+|| color | 字符串 | 十六进制颜色码,或rgba(),如果不指定默认值为 #666 ||
+|| imgUrl | 字符串 | 前面的覆盖图片 ||
+|| backImgUrl | 字符串 | canvas背景图片 ||
+|| width | 字符串 | canvas的宽度，必需和canvas标签中宽度一致 ||
+|| height | 字符串 | canvas的高度，必需和canvas标签中高度一致 ||
+|| radius | 字符串 | 涂抹笔的半径 ||
+|| percent | 数值 | 透明面积占整个画布的百分比，超出此数字显示全部画布 ||
+|| callback | 函数 | 用户自定义的回调函数名称，必填 ||
+
 
 
 例如:
--- gruntfile.js
+++ gruntfile.js
@@ -6,39 +6,21 @@ module.exports = function(grunt){
 		//获取package.json
 		pkg:grunt.file.readJSON('package.json'),
 		//gulify插件的配置信息
-		//pkg.name 版本名称
-		//pkg.version 最新版本
-		//grunt.template.today("yyyy-mm-dd") 日期
 		uglify:{
 			options:{
-				banner:'/*!<%= pkg.name %><%= pkg.version %> 发布日期:<%= grunt.template.today("yyyy-mm-dd") %>*/',
+				banner:'/*!<%= pkg.name %><%= pkg.version %> 发布日期:<%=grunt.template.today("yyyy-mm-dd")%>*/',
 			},
-			build:{
+			dist:{
 				src:"src/js/wipe.js",
 				dest:"dist/js/wipe-<%= pkg.version %>.min.js"
 			},
-			// build2:{
-			// 	src:"src/js/style.js",
-			// 	dest:"build/style-<%= pkg.version %>.min.js"
+			// dist2:{
+			// 	src:"src/js/addclass.js",
+			// 	dest:"dist/addclass-<%= pkg.version %>.min.js"
 			// }
 		},
-		cssmin:{
-			options:{
-				mergeIntoShorthands:false,
-				roundingPrecision:-1
-			},
-			target:{
-				files:[{
-					expand:true,
-					cwd:'src/css',
-					src:['*.css'],
-					dest:'build/css',
-					ext:'.min.css'
-				}]
-			}
-		},
 		clean:{
-			dest:['dist/*','sample/js/*']
+			dest:['dist/*']
 		},
 		jshint:{
 			test:['src/js/wipe.js'],
@@ -47,29 +29,27 @@ module.exports = function(grunt){
 			}
 		},
 		copy:{
-			js:{expand:true,cwd:'dist/js',src:'*.min.js',dest:'sample/js/'}
+			js:{expand:true, cwd:'dist/js',src:'*.js',dest:'sample/js/'}
 		},
 		replace:{
 			example:{
 				src:['sample/index.html'],
 				overwrite:true,
-				replacements:[{
-					from:/wipe-\d[\.]\d[\.]\d\.min/g,
-					to:'wipe-<%= pkg.version %>.min'
-				},{
-					from:/hello\.css/g,
-					to:'hello.min.css'
-				}]
+				replacements:[
+				{
+					from:/\d[\.]\d[\.]\d/g,
+					to:'<%= pkg.version %>'
+				},
+				]
 			}
 		}
 	});
-	// 告诉grunt需要使用插件
+	//告诉grunt需要使用插件
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-text-replace');
-	//告诉grunt当我们输入grunt命令后需要做些什么,有先后顺序
+	//告诉grunt当我们输入grunt命令后需要做些什么，有先后顺序
 	grunt.registerTask('default',['jshint','clean','uglify','copy','replace']);
 };
